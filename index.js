4f6e4a1270b296f92efd9aeb01d4c31f781e1a3d
Legal <offorjohn55@gmail.com>
2024-04-13 15:40:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+// class Car {
+//     constructor(carName, carColor, carModel, carType) {
+//         this.name = carName;
+//         this.color = carColor;
+//         this.model = carModel;
+//         this.type = carType;
+//     }
+
+//     // method module.exports = {car}
+//     changeCarType(newType) {
+//         this.type = newType;
+//     }
+// }
+
+// let myCar = new Car('Toyota', 'Red', 'Corolla', 'Sedan');
+// console.log(myCar.type);
+
+// myCar.changeCarType('SUV');
+// console.log(myCar.type); 
+
+// function addAndDivideByTwo(a, b) {
+//     let sum = a + b;
+//     let result = sum / 2;
+//     return result;
+//    }
+//    let a = 2;
+//    let b = 5;
+//    let result = addAndDivideByTwo(a, b);
+//    console.log(result);
